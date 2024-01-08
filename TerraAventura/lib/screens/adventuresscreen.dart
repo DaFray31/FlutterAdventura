efe6ebe199bc4e34d58902db437beda4753d0902
Benoît <dafray31@picot.dev>
2024-01-08 16:44:53 +0100

    Feature : fetch data from backend

--- TerraAventura/lib/screens/adventuresscreen.dart
+++ TerraAventura/lib/screens/adventuresscreen.dart
@@ -1,9 +1,14 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_map/flutter_map.dart';
 import 'package:latlong2/latlong.dart';
+import 'package:supabase_flutter/supabase_flutter.dart';
 
 class AdventuresScreen extends StatelessWidget {
-  const AdventuresScreen({super.key});
+  const AdventuresScreen({Key? key}) : super(key: key);
+  static final SupabaseClient supabase = SupabaseClient(
+    'https://kdiowwslccpwbzhccjaj.supabase.co',
+    'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImtkaW93d3NsY2Nwd2J6aGNjamFqIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MDMyMzQ2MTMsImV4cCI6MjAxODgxMDYxM30.lHDaL3MYtfNsH5Oop32FfRlkSOhfGoDd34vl0b-4PWA',
+  );
 
   @override
   Widget build(BuildContext context) {
@@ -47,55 +52,106 @@ class AdventuresScreen extends StatelessWidget {
             //map
             child: FlutterMap(
               options: MapOptions(
-                initialCenter: LatLng(51.509364, -0.128928),
-                initialZoom: 9.2,
+                initialCenter: LatLng(45.82975453040051, 1.266962885714927),
+                initialZoom: 10,
               ),
               children: [
                 TileLayer(
                   urlTemplate: 'https://tile.openstreetmap.org/{z}/{x}/{y}.png',
-                  userAgentPackageName: 'com.example.app',
+                  userAgentPackageName: 'flutter_map',
                 ),
                 RichAttributionWidget(
                   attributions: [
                     TextSourceAttribution(
                       'OpenStreetMap contributors',
-                      onTap: () => print('OpenStreetMap attribution tapped!'),
                     ),
                   ],
+                ),
+                StreamBuilder(
+                  stream: supabase.from('monuments').stream(primaryKey: ['id']),
+                  builder: (context, snapshot) {
+                    if (snapshot.hasError) {
+                      return Text('Error: ${snapshot.error}');
+                    }
+
+                    if (snapshot.connectionState == ConnectionState.waiting) {
+                      return Center(
+                        child: CircularProgressIndicator(),
+                      );
+                    }
+
+                    if (!snapshot.hasData || (snapshot.data as List).isEmpty) {
+                      return Text('No data available');
+                    }
+
+                    final data = snapshot.data as List;
+                    List<Marker> markers = [];
+
+                    for (var monumentData in data) {
+                      // Extracting latitude and longitude from the string representation
+                      String pointString = monumentData['localisation'];
+                      List<String> pointValues = pointString
+                          .replaceAll('(', '')
+                          .replaceAll(')', '')
+                          .split(',');
+
+                      double latitude = double.parse(pointValues[1]);
+                      double longitude = double.parse(pointValues[0]);
+
+                      markers.add(
+                        Marker(
+                          point: LatLng(latitude, longitude),
+                          width: 80,
+                          height: 80,
+                          child: FlutterLogo(),
+                        ),
+                      );
+                    }
+
+                    return MarkerLayer(markers: markers);
+                  },
                 ),
               ],
             ),
           ),
-          // Liste de monuments
-          Expanded(
-            child: ListView(
-              children: [
-                ListTile(
-                  leading: Icon(Icons.place),
-                  title: Text('Cathédrale Saint-Étienne'),
-                  subtitle: Text('Cathédrale'),
-                  trailing: Icon(Icons.keyboard_arrow_right),
-                  onTap: () {
-    }
-                ),
-                ListTile(
-                  leading: Icon(Icons.place),
-                  title: Text('Musée des Beaux-Arts'),
-                  subtitle: Text('Musée'),
-                  trailing: Icon(Icons.keyboard_arrow_right),
-                  onTap: () {
-    }
-                ),
-                ListTile(
-                  leading: Icon(Icons.place),
-                  title: Text('Musée de la Résistance'),
-                  subtitle: Text('Musée'),
-                  trailing: Icon(Icons.keyboard_arrow_right),
-                  onTap: () {
 
-                        }
-                ),
-              ],
+          Expanded(
+            child: StreamBuilder(
+              stream: supabase.from('monuments').stream(primaryKey: ['id']),
+              builder: (context, snapshot) {
+                if (snapshot.hasError) {
+                  return Text('Error: ${snapshot.error}');
+                }
+
+                if (snapshot.connectionState == ConnectionState.waiting) {
+                  return Center(
+                    child: CircularProgressIndicator(),
+                  );
+                }
+
+                if (!snapshot.hasData || (snapshot.data as List).isEmpty) {
+                  return Text('No data available');
+                }
+
+                final data = snapshot.data as List;
+                return ListView.builder(
+                  itemCount: data.length,
+                  itemBuilder: (context, index) {
+                    final monument = data[index];
+                    return ListTile(
+                      title: Text(monument['nom'].toString()),
+                      subtitle: Row(
+                        children: [
+                          Text(monument['duree'].toString()),
+                          Text(' - '),
+                          Text(monument['distance'].toString()),
+                          Text('km'),
+                        ],
+                      ),
+                    );
+                  },
+                );
+              },
             ),
           ),
         ],
